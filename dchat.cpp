c155c34d821e2cbdd1868ac3e88905f454831d7b
Karthik Anantha Ram <KarthikAR30@users.noreply.github.com>
2016-03-30 17:13:54 -0400

    Update dchat.cpp

--- dchat.cpp
+++ dchat.cpp
@@ -15,13 +15,15 @@ Authors: Karthik Anantha Ram, Sanjeet Phatak, Sarath Vadakkepat
 #include <unistd.h>
 #include <pthread.h>
 #include <iostream>
-using namespace std;
+#include <sys/types.h>
+#include <ifaddrs.h>
+#include <sstream>
 
-//#define PORT 5000
+using namespace std;
 
 struct Message                                                           
   { 
-    char IncomingMessage[1024];
+    char IncomingMessage[2048];
 
   }msg;
 
@@ -38,6 +40,7 @@ public:
 		string ipAddr;
 		bool isSequencer=false;
 		int leaderPortNum;
+		string seqIpAddr;
 //public:
 		//other methods that are needed
 		//getIpAddr(){
@@ -47,9 +50,6 @@ public:
 };
 
 
-//Declaration of global variables
-
-
 //Method to print error messages
 void error(const char *msg)
 {
@@ -57,67 +57,62 @@ void error(const char *msg)
     exit(-1);
 }
 
-/*
-int createSocket(int portNum)
-{
-	if ((sock=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
-        error("socket");
-    }
-     
-    memset((char *) &si_me, 0, sizeof(si_me));
-     
-    si_me.sin_family = AF_INET;
-    si_me.sin_port = htons(PORT);
-    si_me.sin_addr.s_addr = htonl(INADDR_ANY);
-     
-    if( bind(sock , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1) {
-        error("bind");
-    }
-	
-	return sock;
-}
-
-*/
-/*
-void newGrpChat(void *sock, ChatUser initSeq){
-	
-	int sockfd = *(int*)sock;
-	
-	while(1)
-    {
-        if ((recv_len = recvfrom(sockfd, &msg, sizeof(struct Message), 0, (struct sockaddr *) &si_other, &slen)) == -1) {
-            error("recvfrom()");
-        }
-		
-		cout<<"Message : "<<endl;
+//Method to enable a user join a existing chat
+void existGrpChat(ChatUser newUser){
 		 
-	 }
-    return;
-}*/
+		 cout<<newUser.name<<" joining a new chat on "<<newUser.seqIpAddr<<":"<<newUser.leaderPortNum<<", listening on "<<newUser.ipAddr<<":"<<newUser.portNumber<<endl;
 
-string getIP()	{
-	
-	return "192.168.1.2";
+
+		 struct sockaddr_in si_other;
+  	     int s, i;
+		 socklen_t slen=sizeof(si_other);
+  
+    	 if ( (s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
+        	error("socket");
+    	 }
+ 
+    	memset((char *) &si_other, 0, sizeof(si_other));
+    	si_other.sin_family = AF_INET;
+    	si_other.sin_port = htons(newUser.leaderPortNum);
+     
+    	if (inet_aton(newUser.seqIpAddr.c_str(), &si_other.sin_addr) == 0)  {
+        fprintf(stderr, "inet_aton() failed\n");
+        exit(1);
+		}
+
+		 /* if( inet_pton( AF_INET, newUser.seqIpAddr, &si_other.sin_addr ) <= 0 ) {
+		  perror( "inet_pton for address" );
+		  exit( 99 );
+   	 	 }*/
+		 		
+		while(1){	
+				
+		//cout<<"Enter message"<<endl;
+		//string m="";	
+		//getline(cin,m);
+		strcpy(msg.IncomingMessage, "JOIN");	
+					
+		if (sendto(s, &msg, sizeof(struct Message), 0 , (struct sockaddr *) &si_other, slen)==-1) {
+           error("sendto()");
+        }
+				
+		if (recvfrom(s, &msg, sizeof(struct Message), 0, (struct sockaddr *) &si_other, &slen) == -1) {
+          error("recvfrom()");
+    	}
+	    cout <<msg.IncomingMessage<< endl;
+						
+		}
+
 }
 
-
-int main(int argc, char* argv[]){
- 
- if(argc==2)
-	 {
-		 cout<<"New chat group Initiated"<<endl;
-	 
-		 ChatUser initSeq;
-		 initSeq.isSequencer=true;
-		 initSeq.ipAddr=getIP();
-		 //initSeq.portNumber=1023+(rand()%1000);
-		 initSeq.portNumber=5000;
-		 initSeq.name=argv[1];
-		 initSeq.leaderPortNum=initSeq.portNumber;
-		 initSeq.UIRI++;
-	 
-	 
-	     // TODO 1: move to function 
+//Method to initiate sequencer
+void newGrpChat(ChatUser initSeq){
+		 
+		 cout<<initSeq.name<<" started a new chat, listening on "<<initSeq.ipAddr<<":"<<initSeq.portNumber<<endl;
+	 	 cout<<"Succeeded, current users:"<<endl;
+		 cout<<initSeq.name<<" "<<initSeq.ipAddr<<"."<<initSeq.portNumber<<" (Leader)"<<endl;
+		 cout<<"Waiting for others to join..."<<endl;
+		 	 
 		 struct sockaddr_in si_me, si_other;
 		 int sock, i, recv_len;
 		 socklen_t slen= sizeof(si_other);
@@ -132,14 +127,13 @@ int main(int argc, char* argv[]){
     	 si_me.sin_family = AF_INET;
     	 si_me.sin_port = htons(5000);
     	 si_me.sin_addr.s_addr = htonl(INADDR_ANY);
-     
-    	 if( bind(sock , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1) {
+
+		 if( bind(sock , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1) {
         	error("bind");
     	 }
-		 // END TODO 1
-	   
-	 
-	 	while(1)
+
+
+    	 while(1)
     	{
             if ((recv_len = recvfrom(sock, &msg, sizeof(struct Message), 0, (struct sockaddr *) &si_other, &slen)) == -1) {
               error("recvfrom()");
@@ -147,82 +141,112 @@ int main(int argc, char* argv[]){
 			 
 		    cout << msg.IncomingMessage << endl; 
 			string tmp(msg.IncomingMessage);
-		    string newMessage=tmp+" Hello From the other side\0";	
+			string newMessage;
+			if(strcmp(msg.IncomingMessage,"JOIN") == 0)
+				//"\n"+initSeq.name+" "+initSeq.ipAddr+":"+initSeq.portNumber+" "+"(LEADER)\0"
+				newMessage="Succeeded, current users:\0";	
+			else
+				newMessage="Sorry :(\0";
+			
 			strcpy(msg.IncomingMessage, newMessage.c_str());	
 		    if (sendto(sock, &msg, sizeof(struct Message), 0 , (struct sockaddr *) &si_other, slen)==-1) {
               error("sendto()");
             }
-		 
-			 
+		 	 
 		}
+
+    return;
+}
+
+string getIP()	{
+	
+	struct ifaddrs * ifAddrStruct=NULL;
+    struct ifaddrs * ifa=NULL;
+    void * tmpAddrPtr=NULL;
+
+    getifaddrs(&ifAddrStruct);
+    char key[]="em1";
+    string ip;
+    for (ifa = ifAddrStruct; ifa != NULL; ifa = ifa->ifa_next) {
+        if (!ifa->ifa_addr) {
+            continue;
+        }
+        if (ifa->ifa_addr->sa_family == AF_INET) { // check it is IP4
+            // is a valid IP4 Address
+            tmpAddrPtr=&((struct sockaddr_in *)ifa->ifa_addr)->sin_addr;
+            char addressBuffer[INET_ADDRSTRLEN];
+            inet_ntop(AF_INET, tmpAddrPtr, addressBuffer, INET_ADDRSTRLEN);
+            
+            if(strcmp(ifa->ifa_name,"em1") == 0)
+            {
+            ip=addressBuffer;
+            }
+        } else if (ifa->ifa_addr->sa_family == AF_INET6) { // check it is IP6
+            // is a valid IP6 Address
+            tmpAddrPtr=&((struct sockaddr_in6 *)ifa->ifa_addr)->sin6_addr;
+            char addressBuffer[INET6_ADDRSTRLEN];
+            inet_ntop(AF_INET6, tmpAddrPtr, addressBuffer, INET6_ADDRSTRLEN);
+            //printf("reddy \n");
+            //printf("%s IP Address %s\n", ifa->ifa_name, addressBuffer); 
+        } 
+    }
+    if (ifAddrStruct!=NULL) freeifaddrs(ifAddrStruct);
+    return ip;
+}
+
+
+int main(int argc, char* argv[]){
+ 
+ if(argc==2)
+	 {
+		 
+		 ChatUser initSeq;
+		 initSeq.isSequencer=true;
+		 initSeq.ipAddr=getIP();
+		 //initSeq.portNumber=1023+(rand()%1000);
+		 initSeq.portNumber=5000;
+		 initSeq.name=argv[1];
+		 initSeq.leaderPortNum=initSeq.portNumber;
+		 initSeq.seqIpAddr=initSeq.ipAddr;
+		 initSeq.UIRI++;
+	 	    
+	 	 newGrpChat(initSeq);
 	
  }
 	 else if(argc==3)
 	 {
-		 cout<<"Attempting to join an existing chat......"<<endl;
 		 ChatUser newUser;
 		 newUser.isSequencer=false;
-		 //newUser.ipAddr="sarathlogic";
-		 //newUser.portNumber=1023+(rand()%1000);
+		 newUser.ipAddr=getIP();
+		 newUser.portNumber=5001;
 		 newUser.name=argv[1];
-           
-		 
-		 
-		 // TODO 2: move to function 
-		 
-		 
-		 struct sockaddr_in si_other;
-  	     int s, i;
-		 socklen_t slen=sizeof(si_other);
-  
-    	 if ( (s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
-        	error("socket");
-    	 }
- 
-    	memset((char *) &si_other, 0, sizeof(si_other));
-    	si_other.sin_family = AF_INET;
-    	si_other.sin_port = htons(5000);
-     
-    	if (inet_aton("127.0.0.1", &si_other.sin_addr) == 0)  {
-        fprintf(stderr, "inet_aton() failed\n");
-        exit(1);
-		}
-		 
-		 //END TODO 2
-		
-		while(1){	
-				
-		cout<<"Enter message"<<endl;
-		string m="";	
-		getline(cin,m);
-		strcpy(msg.IncomingMessage, m.c_str());	
-		
-	    
-					
-		if (sendto(s, &msg, sizeof(struct Message), 0 , (struct sockaddr *) &si_other, slen)==-1) {
-           error("sendto()");
-        }
-				
-		if (recvfrom(s, &msg, sizeof(struct Message), 0, (struct sockaddr *) &si_other, &slen) == -1) {
-          error("recvfrom()");
-    	}
-	    cout << "The message was = " << msg.IncomingMessage<< endl;
-			
-			
-		}
+		 newUser.UIRI++;
+
+		 string ipPort=argv[2];
+		 istringstream iss(ipPort);
+		 string token;
+
+		 int cnt=0;
+		 while (getline(iss, token, ':'))
+		 {
+		 	if(cnt==0)
+    			newUser.seqIpAddr=token;
+    		else if(cnt==1)
+    			newUser.leaderPortNum= atoi(token.c_str());
+    		else
+    			break;
+    		cnt++;
+		 }
+
+		 existGrpChat(newUser);
     }
 		 
-		 
-		 
-	 
-	 
+		  
 	 else
 	 {
 		 cout<<"Error:Invalid arguments"<<endl;
 		 return -1;
 	 }
 
-	 //createSocket();
 	 
 }
-
